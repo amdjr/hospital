b5ebc1b8d4ee6551e26bf5657796f8de1d11a555
Adilson Monteiro <adilson_monteiro@outlook.com>
2021-09-01 19:35:09 -0300

    primeiro committ

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Criando um objeto
+        Endereco end1 = new Endereco();
+        Medico med1 = new Medico();
+
+        // imprimir nome do objeto
+
+        System.out.print(end1.rua);
+        System.out.print(med1.crm);
+    }
+}
---
+++ src/Medico.java
@@ -0,0 +1,7 @@
+public class Medico {
+
+    public String crm;
+    public String nomeCompleto;
+    public String telefoneContato;
+    public Endereco endereco;
+}
